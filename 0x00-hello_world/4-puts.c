fa4970bc8440d38342617b837dfc789c955b1a04
rania <rania.ahmed2679@gmail.com>
2023-09-11 17:24:13 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ *main entry point
+ *
+ *description: a C program that print with put fuction
+ *
+ *return: always 0 (success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+xreturn(0);
+}
