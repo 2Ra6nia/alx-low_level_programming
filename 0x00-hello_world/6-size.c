83ac453976b4799bb7df31f47535430dc470ddea
rania <rania.ahmed2679@gmail.com>
2023-09-11 19:17:42 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-printf("size of a char: %lu ", sizeof(char));
-printf("size of a int: %lu ", sizeof(int));
-printf("size of a long int %lu ", sizeof(long int));
-printf("size of a long long int: %ul", sizeof(long long int));
-printf("size of afloat: %lu ", sizeof(float));
+printf("size of a char: %lu byte(s)\n ", sizeof(char));
+printf("size of a int: %lu byte(s)\n ", sizeof(int));
+printf("size of a long int %lu byte(s)\n ", sizeof(long int));
+printf("size of a long long int: %ul byte(s)\n ", sizeof(long long int));
+printf("size of a float: %lu byte(s)\n ", sizeof(float));
 return (0);
 }
