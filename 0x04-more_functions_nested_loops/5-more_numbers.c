0d804f09aa7c55db71f4d4ea82603a9a423c0ea4
rania <rania.ahmed2679@gmail.com>
2023-09-14 12:47:23 -0700

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* more_numbers - print numbers (0 - 14) 10 times
+* only use _putchar three times
+* Return: always 0 (success)
+*/
+
+void more_numbers(void)
+{
+int num;
+int rows;
+int count;
+for (rows = 1; rows <= 10; rows++)
+{
+for (count = 0; count <= 14; count++)
+{
+num = count;
+if (count > 9)
+{
+_putchar(1 + 48);
+num = count % 10;
+}
+_putchar(num + 48);
+}
+_putchar('\n');
+}
+}
