b04655b6b6674670f276ae2193222a35f36c43c4
rania <rania.ahmed2679@gmail.com>
2023-09-13 18:28:45 -0700

    a

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,6 +7,7 @@
 
 int main(void)
 {
+  
 int i;
 printf("Infinite loop incoming :(\n");
 
@@ -16,6 +17,8 @@ i = 0;
 /*{*/
 /* putchar(i);*/
 /*}*/
+ 
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
