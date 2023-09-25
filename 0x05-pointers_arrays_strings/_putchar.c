4d14866874dbbd9bda26962e10ba9db2e3719feb
rania <rania.ahmed2679@gmail.com>
2023-09-24 18:24:59 -0700

    a

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * _putchar - print charchter in one line
@@ -8,5 +8,5 @@
 */
 int _putchar(char c)
 {
-return (write(1, %c ,1));
+return (write(1, %c, 1));
 }
