af5b860bc8508bacb0927792e25bbc35f62f85bd
rania <rania.ahmed2679@gmail.com>
2023-09-24 17:16:52 -0700

    a

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * _putchr - print charchter in one line
+ * @c: the characher that will print
+ * Return: on success 1.
+*/
+int _putchar(char c)
+{
+return (write(1, %c, 1)):
+}
