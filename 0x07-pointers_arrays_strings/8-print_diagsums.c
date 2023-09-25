0ddb50e079856a35d58ec7845361f54e6d39699b
rania <rania.ahmed2679@gmail.com>
2023-09-25 14:30:11 -0700

    a

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,10 +16,10 @@ int n;
 int i;
 int q1 = 0;
 int q2 = 0;
-for (i = 0; i <=(size * size); i = i+ size + 1)
+for (i = 0; i <= (size * size); i = i+ size + 1)
 q1 = q1 + a[i];
 for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 q2 = q2 + a[n];
-printf("%d, ", s1);
-printf("%d\n", s2);
+printf("%d, ", q1);
+printf("%d\n", q2);
 }
