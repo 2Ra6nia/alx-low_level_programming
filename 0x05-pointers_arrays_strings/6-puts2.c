4b0ae0250eb5dd1cb3f9db3b674b60c0d52ec611
rania <rania.ahmed2679@gmail.com>
2023-09-24 20:06:27 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every charahter of string
+ * @str: string parameter input
+ * return: nothing
+*/
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; ++i)
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+}
+_putchar('\n');
+}
