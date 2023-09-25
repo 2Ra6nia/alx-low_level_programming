835dccc847e819cda5472f60501dbc5031ea3bc2
rania <rania.ahmed2679@gmail.com>
2023-09-25 08:40:24 -0700

    a

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-*_strpbrk - searches a string of any of a set of bytes
+* *_strpbrk - searches a string of any of a set of bytes
 * @s: string
 * @accept: string to match
 * Return: zero (success)
 */
 
-char _strpbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 int i;
 int j;
