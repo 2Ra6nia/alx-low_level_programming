86f7bcd076b62b12737ed17601572b98583fa2fc
rania <rania.ahmed2679@gmail.com>
2023-09-28 05:40:55 -0700

    string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include  "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: poinetr that store string
+ * Return: 0 (success)
+*/
+
+void _puts_recursion(char *s)
+{
+char c;
+*s = c;
+_putchar("%c", c);
+return (0);
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints the  charchter in one line
+ * @c: the charchter
+ * Return: c value
+*/
+
+char _putchar(char c)
+{
+return (write(1, % c, 1));
+}
