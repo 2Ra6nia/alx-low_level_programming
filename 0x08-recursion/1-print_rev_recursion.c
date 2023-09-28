bc6e59ddbb86da0c3c41b977dc43c2bb8780d39c
rania <rania.ahmed2679@gmail.com>
2023-09-28 07:00:48 -0700

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#includ "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: the pointer thar are store string
+ * Return: nothing
+*/
+
+
+void _print_rev_recursion(char *s)
+{
+char c;
+if (*s == '\0')
+{
+return;
+}
+c = *s;
+_putchar(c);
+_print_rev_recursion(s - 1);
+}
