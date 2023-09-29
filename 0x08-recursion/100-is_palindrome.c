ef54dd887df3c97a6baf6c58b3e288022576c202
rania <rania.ahmed2679@gmail.com>
2023-09-29 08:33:35 -0700

    a

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_palindrome - check if string is palindrome
+ * @s: the string
+ * Return: 1 if a string is a palindrome
+ * and 0 if not.
+*/
+
+int is_palindrome(char *s)
+{
+char c;
+c = *s;
+if (*s == c--)
+{
+return (1);
+}
+return (0);
+}
