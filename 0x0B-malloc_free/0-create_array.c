ffa7aca9153a1423e165670750ed1de2bffb4f96
rania <rania.ahmed2679@gmail.com>
2023-10-03 14:57:52 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,19 +13,19 @@
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
-char *array;
+
 if (size == 0)
 {
 return (NULL);
 }
-*array = malloc(sizeof(char) * size);
+char *array = malloc(sizeof(char) * size);
 if (array  == NULL)
 {
 return (NULL);
 }
 for (i = 0; i < size; i++)
 {
-array[i] = c;
+array[size] = c;
 }
 free(array);
 return (array);
