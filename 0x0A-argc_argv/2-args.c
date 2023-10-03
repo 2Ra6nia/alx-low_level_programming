6c89bc58981e5762fff760dcf07f1ac7e152225c
rania <rania.ahmed2679@gmail.com>
2023-10-02 19:19:10 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *description: prints the argument its recive
+ *@argc: argument counter
+ *@argv: an array
+ *return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
