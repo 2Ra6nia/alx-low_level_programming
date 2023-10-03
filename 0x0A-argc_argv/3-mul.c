3875c0f36534e8f77e570481f3ba6495fcd72c9b
rania <rania.ahmed2679@gmail.com>
2023-10-02 19:58:43 -0700

    a

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: program that mlutipules two numbers
+ * @argc: argument counter
+ * @argv: an array
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+int mu;
+int i;
+if (argc <= 0)
+{
+printf("error");
+}
+for (i = 1; i < argc; i++)
+mu *= atoi(argv[i]);
+printf("%d\n", mu);
+return (0);
+}
