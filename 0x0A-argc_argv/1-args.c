9048336dc64f51d020ce447d51aa4a25343b10eb
rania <rania.ahmed2679@gmail.com>
2023-10-02 18:24:28 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: prints the number of arguments
+ *@argc: argunment counter
+ *@argv: an array
+ *Return: 0 (success)
+*/
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
