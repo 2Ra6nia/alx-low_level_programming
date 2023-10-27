292fd78074621a12d7b1c85b192996f969995c90
2Ra6nia <rania.ahmed2679@gmail.com>
2023-10-27 12:01:52 -0700

    set

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
 * Return: 1 (success), -1  (error)
 */
 
-int set_bit(unsigned long int *n, unsigned int index)  
+int set_bit(unsigned long int *n, unsigned int index)
 {
 if (index >= sizeof(n) * 8)
 return (-1);
