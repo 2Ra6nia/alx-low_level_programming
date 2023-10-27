f255cbdd4c8ef702da1d3c68a0a2c56855113c9f
2Ra6nia <rania.ahmed2679@gmail.com>
2023-10-27 11:33:00 -0700

    filp

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,11 +7,11 @@
 * Return: 1 if success or -1  (error)
 */
 
-int clear_bit(unsigned long int *n, unsigned int index)  
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 if (index >= sizeof(n) * 8)
 return (-1);
-if(*n & 1L << index)
+if (*n & 1L << index)
 *n ^= 1L << index;
 return (1);
 }
